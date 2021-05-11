a34681409268202c0d900313b57c0323c130eae3
Guillermo <guillermo.baban@hotmail.com>
2021-05-11 11:37:05 -0300

    Proyeto para practicar

--- index.js
+++ index.js
@@ -2,12 +2,12 @@ document.querySelector("#tet").addEventListener("click", navAct);
 document.querySelector(".bolitas").addEventListener("click", navActB);
 document.addEventListener("click", porFuera);
 document.querySelector("#btn-B").addEventListener("click", accionB);
-document.querySelector("#usuarioI").addEventListener("keydown", test);
+document.querySelector("#usuarioI").addEventListener("keydown", enter);
 
 const navL = document.querySelector(".caja-des");
 const navB = document.querySelector(".segun-des");
 
-function test(e) {
+function enter(e) {
   let usuarioIn = document.querySelector("#usuarioI").value;
   if (e.keyCode === 13) {
     location.href = `https://www.google.com.ar/search?q=${usuarioIn}&sxsrf=ALeKk03c7rPfHqUQeOTFuT2RoKYjhNTttA%3A1620419104649&source=hp&ei=IKKVYK7hJIO25OUPi7eTaA&iflsig=AINFCbYAAAAAYJWwMGZdP_pI7xtqlgYQBtCqZYuzooln&oq=aoeao&gs_lcp=Cgdnd3Mtd2l6EAMyBAgjECcyBAguEEMyBAguEEMyBAguEEMyBwgAEIcCEBQyAggAMgIIADICCAAyAggAMgIIADoFCAAQkQI6BAgAEEM6CAguEMcBEKMCOgcILhCHAhAUOgIILlCOA1iwBWD8BWgAcAB4AIAB9wGIAesEkgEFMC4yLjGYAQCgAQGqAQdnd3Mtd2l6&sclient=gws-wiz&ved=0ahUKEwiu8ffIs7jwAhUDG7kGHYvbBA0Q4dUDCAc&uact=5`;
